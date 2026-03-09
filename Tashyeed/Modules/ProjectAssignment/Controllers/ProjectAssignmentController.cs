d7450ebdc62e0dccb21f0fa53dea344241945309
ebrah <ebrah@LAPTOP-TM8TPK8A>
2026-03-09 20:39:00 +0300

    i think it's a last version of src maybe add tests

--- Tashyeed/Modules/ProjectAssignment/Controllers/ProjectAssignmentController.cs
+++ Tashyeed/Modules/ProjectAssignment/Controllers/ProjectAssignmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Tashyeed.Infrastructure.Identity;
@@ -9,6 +10,7 @@ using Tashyeed.Web.Modules.ProjectAssignment.ViewModels;
 
 namespace Tashyeed.Web.Modules.ProjectAssignment.Controllers
 {
+    [Authorize(Roles = RoleNames.Admin)]
     public class ProjectAssignmentController : Controller
     {
         private readonly IProjectAssignmentService _assignmentService;
